7c49837c0d00afdafd24d40e1b0548829b014479
Dhruv Sheth <123584784+Dhruvnet@users.noreply.github.com>
2024-03-26 10:31:57 +0530

    Add files via upload

---
+++ src/components/sidebar/components/SidebarCard.js
@@ -0,0 +1,19 @@
+import {
+  Button,
+  Flex,
+  Image,
+  Link,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import logoWhite from "assets/img/layout/logoWhite.png";
+import React from "react";
+
+export default function SidebarDocs() {
+  const bgColor = "linear-gradient(135deg, #868CFF 0%, #4318FF 100%)";
+  const borderColor = useColorModeValue("white", "navy.800");
+
+  return (
+    <></>
+  );
+}
