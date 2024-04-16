98cddc7997c8c7d6fbe6a551df42baa8613804ce
Dhruv Sheth <123584784+Dhruvnet@users.noreply.github.com>
2024-04-16 22:30:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ ReactDOM.render(
 						<Route path={`/auth`} component={AuthLayout} />
 						<Route path={`/admin`} component={AdminLayout} />
 						<Route path={`/rtl`} component={RtlLayout} />
-						<Redirect from='/' to='/auth' />
+						<Redirect from='/' to='/admin' />
 					</Switch>
 				</HashRouter>
 			</ThemeEditorProvider>
